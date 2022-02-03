374bf98f3414fa66bdb83690fdd69871304896f2
Alejandro Ojeda Maruny <ojedamaruny@gmail.com>
2022-02-03 18:43:54 +0100

    🎉 First commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
